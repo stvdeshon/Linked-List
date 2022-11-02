75acfbf069e5050953558a886240eb75811bc0d1
stvdeshon <stvdeshon@gmail.com>
2022-11-02 08:52:54 -0600

    Initial commit

---
+++ LinkedList.js
@@ -0,0 +1,13 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+}
+
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
